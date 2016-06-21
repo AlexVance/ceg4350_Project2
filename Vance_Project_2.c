e29a565ae3e2c2519a99870d49a8b0a4b560671b
AlexVance <alexvance17@gmail.com>
2016-06-21 11:25:07 -0400

    periodic update

--- Vance_Project_2.c
+++ Vance_Project_2.c
@@ -8,33 +8,49 @@
 #include <stdlib.h>
 #include <string.h>
 
-void fifo(int num_frames, int * page_refs){
+void fifo(int num_frames, int * page_refs, int page_refs_size){
 
 int count = 0;
 int frames[num_frames];
 int num_pages;
-int temp_count[sizeof(page_refs)/ sizeof(page_refs[0])];
+int temp_count[page_refs_size];
 int k, i, j, temp = 0;
-printf("The size is: %d", (sizeof(page_refs)/ sizeof(page_refs[0])));
+
+//fill temp array with 0s
+for (i = 0; i < page_refs_size; ++i)
+{
+	temp_count[i] = -1;
+}
+
 //count page numbers 
-for (i = 0; i < (sizeof(page_refs)/ sizeof(page_refs[0])); i++){
-	for (j = 0; j < (sizeof(temp_count)/ sizeof(temp_count[0])); j++){
+for (i = 0; i < page_refs_size; i++){
+
+	//loop through temp_count and check values
+	for (j = 0; j < page_refs_size; j++){
 		if(temp_count[j] == page_refs[i]){
 			temp = 1;
+			break;
 		}
 	}
-
+	
+	//there weren't any matches, add it to the list, increment count and move on
 	if(temp == 0){
-			for (j = 0; j < (sizeof(temp_count)/ sizeof(temp_count[0])); j++){
-				if(temp_count[j] == 0){
+			for (j = 0; j < page_refs_size; j++){
+				printf("\nTemp_count[j] is: %d", temp_count[j]);
+				if(temp_count[j] == -1){
 					temp_count[j] = page_refs[i];
 					num_pages++;
+					break;
 				}	
 			}
 		}
 }
+for (i = 0; i < page_refs_size; ++i)
+{
+	printf("%d, ",temp_count[i]);
+}
 
-printf("Total number of pages: %d", num_pages);
+printf("\nTotal number of pages: %d", num_pages);
 
 //gather frames into array
 for (i = 0; i < num_frames; i++){
@@ -89,6 +105,7 @@ int main(int argc, char *argv[]) {
 
 	int num_frames;
 	int page_refs[argc - 1];
+	int page_refs_size;
 	int i;
 
 	//if arguments were provided get those values
@@ -99,9 +116,12 @@ int main(int argc, char *argv[]) {
 		for(i = 2; i < argc; i++){
 			page_refs[i-2] = atoi(argv[i]);
 		}
+
+		//save size of page_refs array for future access
+		page_refs_size = (sizeof(page_refs)/ sizeof(page_refs[0]))-1;
 	}
 
-	fifo(num_frames, page_refs);
+	fifo(num_frames, page_refs, page_refs_size);
 	//optimal(num_frames, page_refs);
 	//lru(num_frames, page_refs); 
 	//lfu(num_frames, page_refs);
