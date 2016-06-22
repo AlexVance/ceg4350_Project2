84f766b299e7ef6991df1251d9998f1caab2b819
AlexVance <alexvance17@gmail.com>
2016-06-22 19:01:21 -0400

    updated code

--- Vance_Project_2.c
+++ Vance_Project_2.c
@@ -8,85 +8,67 @@
 #include <stdlib.h>
 #include <string.h>
 
-void fifo(int num_frames, int * page_refs, int page_refs_size){
+int fifo(int num_frames, int * page_refs, int page_refs_size){
 
-int count = 0;
+int page_faults = 0;
 int frames[num_frames];
-int num_pages;
-int temp_count[page_refs_size];
-int k, i, j, temp = 0;
-
-//fill temp array with 0s
-for (i = 0; i < page_refs_size; ++i)
-{
-	temp_count[i] = -1;
-}
+int num_pages = 0;
+int k, i, j;
 
 //count page numbers 
 for (i = 0; i < page_refs_size; i++){
 
-	//loop through temp_count and check values
-	for (j = 0; j < page_refs_size; j++){
-		if(temp_count[j] == page_refs[i]){
-			temp = 1;
-			break;
-		}
-	}
-	
-	//there weren't any matches, add it to the list, increment count and move on
-	if(temp == 0){
-			for (j = 0; j < page_refs_size; j++){
-				printf("\nTemp_count[j] is: %d", temp_count[j]);
-				if(temp_count[j] == -1){
-					temp_count[j] = page_refs[i];
-					num_pages++;
-					break;
-				}	
-			}
-		}
+  //loop through temp_count and check values
+  for (j = 0; j < i; j++){
+    if(page_refs[i] == page_refs[j]){
+      break;
+    }
+  }
+  //there weren't any matches,
+  if(i == j){
+    num_pages++;
+  }
 }
-for (i = 0; i < page_refs_size; ++i)
-{
-	printf("%d, ",temp_count[i]);
-}
-
-printf("\nTotal number of pages: %d", num_pages);
 
 //gather frames into array
 for (i = 0; i < num_frames; i++){
-	frames[i] = -1;
+  frames[i] = -1;
 }
+
+printf("Number of Pages: %d\n", num_pages);
+printf("Number of Frames: %d\n", num_frames);
+printf("# values entered: %d\n", page_refs_size);
 
 printf("\n Displaying Distribution-----------\n");
 for(i = 0; i < num_pages; i++){
-		k = 0;
+    k = 0;
 
-		for (j = 0; j < num_frames; ++j){
-			if(page_refs[i] == frames[i]){
-				k++;
-				count--;
-			}
-		}
+    for (j = 0; j < num_frames; j++){
+      if(page_refs[i] == frames[i]){
+        k++;
+        page_faults--;
+      }
+    }
 
-		count++;
+    page_faults++;
 
-		if(count <= num_frames && k == 0) {
-			frames[i] = page_refs[i];
-		}else if(k == 0) {
-			frames[(count-1)% num_frames] = page_refs[i];
-		}
+    if(page_faults <= num_frames && k == 0) {
+      frames[i] = page_refs[i];
+    }else if(k == 0) {
+      frames[(page_faults-1)% num_frames] = page_refs[i];
+    }
 
-		printf("\n\t***********************************\n");
-		for (i = 0; i < num_frames; ++i)
-		{
-			printf("\t");
-			printf("(%d)\t", frames[i]);
-		}
+    printf("\n\t***********************************\n");
+    for (j = 0; j < num_frames; j++)
+    {
+      printf("\t");
+      printf("(%d)\t", frames[j]);
+    }
 }
 
 //show total page faults
-printf("\nTotal Page Faults %d", count);
-
+printf("\nTotal Page Faults %d\n", page_faults);
+return 0;
 }//end fifo
 
 void optimal(int num_frames, char page_refs){
@@ -103,27 +85,27 @@ void lfu(int num_frames, char page_refs){
 
 int main(int argc, char *argv[]) {
 
-	int num_frames;
-	int page_refs[argc - 1];
-	int page_refs_size;
-	int i;
+  int num_frames;
+  int page_refs[argc - 1];
+  int page_refs_size;
+  int i;
 
-	//if arguments were provided get those values
-	if(argc > 1){
-		num_frames = atoi(argv[1]);
-		
-		//gather list of page references
-		for(i = 2; i < argc; i++){
-			page_refs[i-2] = atoi(argv[i]);
-		}
+  //if arguments were provided get those values
+  if(argc > 1){
+    num_frames = atoi(argv[1]);
+    
+    //gather list of page references
+    for(i = 2; i < argc; i++){
+      page_refs[i-2] = atoi(argv[i]);
+    }
 
-		//save size of page_refs array for future access
-		page_refs_size = (sizeof(page_refs)/ sizeof(page_refs[0]))-1;
-	}
+    //save size of page_refs array for future access
+    page_refs_size = (sizeof(page_refs)/ sizeof(page_refs[0]))-1;
+  }
 
-	fifo(num_frames, page_refs, page_refs_size);
-	//optimal(num_frames, page_refs);
-	//lru(num_frames, page_refs); 
-	//lfu(num_frames, page_refs);
-	return 0;
+  fifo(num_frames, page_refs, page_refs_size);
+  //optimal(num_frames, page_refs);
+  //lru(num_frames, page_refs); 
+  //lfu(num_frames, page_refs);
+  return 0;
 }
