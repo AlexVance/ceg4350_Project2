eb0f79d9d92cc5511364c3b5f1bf6ad0bd89196f
AlexVance <AlexVance17@Gmail.com>
2016-07-03 15:46:59 -0400

    Uploading my final work, not finished but what is there works

--- temp.c
+++ temp.c
@@ -1,59 +1,126 @@
-void optimal(){
-int check_frame[10],i,j,k,page_faults,num_frames,flag,temp,current,c,dist,max,m,cnt,p,x;
-page_faults=0;
-dist=0;
-k=0;
 
-printf(“\nEnter frame size:”);
-scanf(“%d”,&num_frames);
+//LRU
+// void display();
+// int page_refs[12]={2,3,2,1,5,2,4,5,3,2,5,2},i,j,fs[3];
+// int index,k,l,flag1=0,flag2=0,pf=0,frsize=3;
+//
+// for(i=0;i<3;i++){
+// fr[i]=-1;
+// }
+//
+// for(j=0;j<12;j++){
+// flag1=0,flag2=0;
+//    for(i=0;i<3;i++){
+//       if(fr[i]==page_refs[j]){
+//          flag1=1;
+//          flag2=1;
+//          break;
+//       }
+//    }
+//    if(flag1==0){
+//       for(i=0;i<3;i++){
+//          if(fr[i]==-1){
+//             fr[i]=page_refs[j];
+//             flag2=1;
+//             break;
+//          }
+//       }
+//    }
+//    if(flag2==0){
+//       for(i=0;i<3;i++)
+//       fs[i]=0;
+//       for(k=j-1,l=1;l<=frsize-1;l++,k--){
+//          for(i=0;i<3;i++){
+//             if(fr[i]==page_refs[k])
+//             fs[i]=1;
+//          }
+//       }
+//       for(i=0;i<3;i++){
+//          if(fs[i]==0)
+//          index=i;
+//       }
+//       fr[index]=page_refs[j];
+//       pf++;
+//    }
+//    display();
+//    }
+//    printf("\n no of page faults :%d",pf);
+//    getch();
+//    }
+//    void display()
+//    {
+//       int i;
+//       printf("\n");
+// for(i=0;i<3;i++)
+// printf("\t%d",fr[i]);
+// }
 
-//initilizing distance and frame array
-for(i=0;i<num_frames;i++){
-check_frame[i]=0;
-frames[i]=-1;
+//LFU
+#include<stdio.h>
+int i,j=1,s,k,l,re[30],p[10],ch,no,nr,c,a1=0,a,line=6,nk;
+struct
+{
+          int st,l,ps,pos;
+}opr;
+main()
+{
+          clrscr();
+          printf("Enter length of reference string:");
+          scanf("%d",&nr);
+          printf(" Enter reference string:");
+          for(i=1;i<=nr;i++)
+          scanf("%d",&re[i]);
+          printf("\n Enter number of frames:");
+          scanf("%d",&no);
+          clrscr();
+          for(i=1;i<=no;i++)
+          p[i]=-1;
+          opr.st=100;
+          for(i=1;i<=nr;i++)
+          {
+            a1=0;
+            opr.st=100;
+            opr.pos=100;
+
+            for(c=1;c<=no;c++)
+               if(re[i]==p[c])
+               a1++;
+                  if(a1==0){
+                     if(j<=no){
+                        p[j]=re[i];
+                        j++;
+                     }else{
+                     for(k=1;k<=no;k++){
+                        a=0;
+                        for(ch=i-1;ch>0;ch--){
+                           if(p[k]==re[ch]){
+                              a++;
+                              nk=ch;
+                           }
+                        }
+                        if(a>1){
+                           if(opr.st>a){
+                              opr.st=a;
+                              opr.ps=k;
+                           }else
+                              if(opr.st==a){
+                                 if(opr.pos>ch)
+                                     opr.ps=k;
+                              }
+                        }else
+                           if(a==1){
+                              if(opr.pos>nk){
+                                 opr.pos=nk;
+                                 opr.ps=k;
+                                 opr.st=a;
+                              }
+                           }
+                     }
+
+                     p[(opr.ps)]=re[i];
+                     }
+                   }
+                   display(no,p,i);
+          }
+          printf("\n");
 }
-
-for(i=0;i<num_pages;i++){
-flag=0;
-temp=page_refs[i];
-   for(j=0;j<num_frames;j++){
-         if(temp==frames[j]){
-            flag=1;
-            break;
-         }
-      }
-      if((flag==0)&&(k<num_frames)){
-         page_faults++;
-         frames[k]=temp;
-         k++;
-      }else if((flag==0)&&(k==num_frames)){
-         page_faults++;
-         for(cnt=0;cnt<num_frames;cnt++){
-            current=frames[cnt];
-            for(c=i;c<num_pages;c++){
-               if(current!=page_refs[c])
-               check_frame[cnt]++;
-               else
-               break;
-            }
-         }
-         max=0;
-         for(m=0;m<num_frames;m++){
-            if(check_frame[m]>max){
-               max=check_frame[m];
-               p=m;
-            }
-         }
-         frames[p]=temp;
-      }
-
-printf(“\n”);
-
-   for(x=0;x<num_frames;x++){
-      printf(“%d\t”,frames[x]);
-   }
-}
-
-printf(“\nTotal number of faults=%d”,page_faults);
-}
-2,3,2,1,5,2,4,5,3,2,5,2
